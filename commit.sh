5f904792f05199b240f450f469daf774ee5fd708
DUT-Jiang <gaojiang@mail.dlut.edu.cn>
2018-12-13 18:40:08 +0800

    2018年 12月 13日 星期四 18:40:08 +08

--- commit.sh
+++ commit.sh
@@ -6,5 +6,5 @@ git commit -m "`date`"
 git checkout master
 git merge newbranch
 git diff
-echo gaojiang@mail.dlut.edu.cn | echo git8494+ |git push -u origin master
+git push -u origin master
 git branch -D newbranch
